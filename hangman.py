f3a573e0d4efb27ea9dac75fc27dd5066e77943d
N0790170 <tavian.taylor2018@my.ntu.ac.uk>
2020-06-02 11:18:32 +0100

    basic functionality of the game complete

--- hangman.py
+++ hangman.py
@@ -1,38 +1,74 @@
 #Hangman by Tavian Taylor
 import random
+import sys
 words = ["unpopular","gargantuan", "dragon","saline","serendipity","aglet","nauseant","kidding","mississippi"]
 gameOver = False
+totalGuesses = 7
 def chooseWord():
     randomWord = random.randint(0,len(words) - 1)
-    return words[randomWord]
+    word = words[randomWord]
+    wordAsList = []
+    for i in range (len(word)):
+        wordAsList.append(word[i])
+    return wordAsList
+
 def toUnderscores(word):
-    wordLength = len(word)
-    hangString = ""
-    # for x in range (wordLength):
-    hangString = '_' * wordLength
-    return hangString
+    emptyWordList = []
+    for i in range(len(word)):
+        emptyWordList.append('_')
+    return emptyWordList
+
 def getInput():
     guess = input("Enter a guess: ")
-    if checkGuessValid(guess):
-        return guess
-def checkGuessValid(guess):
-    if len(guess) > 1:
-        print("invalid input")
-        return False
+    return guess
+def printWord(emptyWord):
+    print(*emptyWord)
+def checkForAnswer(word,guess,emptyWord,totalGuesses):
+    correctGuess = False
+    if len(guess) == 1:
+        for i in range(len(word)):
+            if guess == word[i]:
+                emptyWord[i] = guess
+                correctGuess = True
+        if correctGuess == False:
+            totalGuesses = decrementGuesses(totalGuesses)
+        
     else:
-        return True
-def checkForAnswer(word,guess,emptyWord):
-    newEmptyWord = ""
+        wordAsString = convertToString(word)
+        if guess == wordAsString:
+            gameWon(totalGuesses)
+        else:
+            totalGuesses = decrementGuesses(totalGuesses)
+    return emptyWord,totalGuesses
+        
+def convertToString(word):
+    newWord = ""
     for i in range(len(word)):
-        if guess == word[i]:
-            newEmptyWord = emptyWord[:i] + guess + emptyWord[i+1:]
-            
-    print(newEmptyWord) #CONVERT STRING TO LIST         
-                    
+        newWord = newWord + word[i]
+    return newWord
+def gameWon(totalGuesses):
+    print("WINNER WINNER CHICKEN DINNER")
+    print("You won with " + str(totalGuesses) + " guesses to spare")
+    sys.exit()
+def decrementGuesses(totalGuesses):
+    totalGuesses = totalGuesses - 1
+    return totalGuesses  
+def gameOverCheck(totalGuesses):
+    if totalGuesses == 0:
+        return True
+    else:
+        return False
+def gameLost():
+    print("GAME OVER!!!")
+    print("Better luck next time!")                    
 word = chooseWord()
 emptyWord = toUnderscores(word)
+print(word)
 print("Welcome to hangman!")
 while not gameOver:
+    printWord(emptyWord)
+    print("Remaing guesses: " + str(totalGuesses))
     guess = getInput()
-    checkForAnswer(word,guess,emptyWord)
-
+    emptyWord,totalGuesses = checkForAnswer(word,guess,emptyWord,totalGuesses)
+    gameOver = gameOverCheck(totalGuesses)
+gameLost()
